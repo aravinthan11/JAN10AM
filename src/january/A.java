88e801cfd63ffaddc7c235d034078269a28c85b2
aravinthan11 <aravinthan1412111@gmail.com>
2020-01-30 14:25:01 +0530

    i have done coding

---
+++ src/january/A.java
@@ -0,0 +1,15 @@
+package january;
+
+public class A {
+	private void saving() {
+		System.out.println("THE SAVINGS DEPOSIT IS 5%");
+		}
+		private void fixed() {
+		System.out.println("THE FIXED DEPOSIT IS 7%");
+		}
+		public static void main(String[] args) {
+			A b=new A();
+			b.saving();
+			b.fixed();
+
+}}
